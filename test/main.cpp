59413ae80f2a4c6303c5b7038cf0dce8801e2a1a
yamadaibuki <hensati11@gmail.com>
2025-05-24 19:51:09 +0900

    nlohmannjsonが正常に作動するようにコードを書き直した

--- test/main.cpp
+++ test/main.cpp
@@ -1,14 +1,29 @@
 #include "DxLib.h"
-#include<fstream>
-//#include<nlohmann/json.hpp>
-#include <iostream>
+#include "nlohmannJson.h"
+
+struct JsonData
+{
+	int hpMax;
+	int hpLimiteOver;
+
+	float initUIPositionX;
+	float initUIPositionY;
+};
+
+inline void from_json(const nlohmann::json& json, JsonData& jsondata)
+{
+	json.at("hp_max").get_to(jsondata.hpMax);
+	json.at("hp_limite_over").get_to(jsondata.hpLimiteOver);
+	json.at("init_ui_position_x").get_to(jsondata.initUIPositionX);
+	json.at("init_ui_position_y").get_to(jsondata.initUIPositionY);
+}
 
 void DrawHP(int hp, int hpMax, VECTOR initPosition)
 {
 	int color = GetColor(255, 255, 255);
-	DrawBox(initPosition.x, initPosition.y,
+	DrawBoxAA(initPosition.x, initPosition.y,
 		initPosition.x + 200, initPosition.y + 20, color, FALSE);	//枠を描画
-	DrawBox(initPosition.x, initPosition.y,
+	DrawBoxAA(initPosition.x, initPosition.y,
 		initPosition.x + 200 * hp / hpMax, initPosition.y + 20, color, TRUE);	//HPゲージを描画
 }
 
@@ -16,40 +31,23 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 {
 	ChangeWindowMode(TRUE), DxLib_Init(), SetDrawScreen(DX_SCREEN_BACK); //ウィンドウモード変更と初期化と裏画面設定;
 
-	using json = nlohmann::json;
-	using namespace std;
+	int currentHp = 0;
 
-	//json j2 = {
-	//	{"hpmax",180},
-	//	//{"initPos",{"x":100.0,"y" : 100.0,"z" : 100.0}}
-	//};
-
-	ifstream stream("data.json");//jsonファイルを開く
-
-	if (!stream.is_open())
-		throw new std::exception("Failed open file.");
-
-	if (!json::accept(stream))
-		throw new std::exception("jsonのフォーマットが不正");
-
-	stream.seekg(0, ios::beg);		//なんか先頭に戻すらしい(ないとバグる)
-	json j = json::parse(stream);	//parseはjsonファイルからC++で使えるように変換するもの
-	stream.close();					//パースしたら閉じておｋ
-
-	int hp = 0;
-
-	ifstream inFile("output.json");
-	json fileJson;
-	inFile >> fileJson;
-	VECTOR loadedVector = fileJson.get<VECTOR>();
+	std::ifstream stream("data.json");
+	JsonData jsonData = nlohmann::json::parse(stream).get<JsonData>();
+	//JsonData jsonData = LoadData<JsonData>("data.json");
+	VECTOR position;
+	position.x = jsonData.initUIPositionX;
+	position.y = jsonData.initUIPositionY;
+	position.z = 0.0f;
 
 	while (!ScreenFlip() && !ProcessMessage() && !ClearDrawScreen() && !CheckHitKey(KEY_INPUT_ESCAPE)) 
 	{
-		DrawHP(hp, j.at("hpmax").get<int>(), loadedVector);//.atは要素数を指定するもの。getでポインター渡しみたいなやつ。<>で型を指定。
-		hp++;
-		if (hp >= j.at("hpmax").get<int>())
+		DrawHP(currentHp, jsonData.hpMax, position);//.atは要素数を指定するもの。getでポインター渡しみたいなやつ。<>で型を指定。
+		currentHp++;
+		if (currentHp >= jsonData.hpMax)
 		{
-			hp = 0;
+			currentHp = 0;
 		}
 	}
 
