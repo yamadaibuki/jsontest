e62635d3d8c0fd3ccadf977addb46349b2efc924
yamadaibuki <hensati11@gmail.com>
2025-05-24 19:50:33 +0900

    jsonのヘッダーファイルを作成

--- test/nlohmannJson.h
+++ test/nlohmannJson.h
@@ -1,9 +1,34 @@
 #pragma once
 #include <string>
+#include <fstream>
+#include<nlohmann/json.hpp>
 
-using namespace std;
+// 書き直し！！
 
 template<typename T>
-inline T LoadDataAsync(const string& fileName) {
-	//json jsonData
+inline T LoadData(const std::string& fileName)
+{
+	std::ifstream stream(fileName);//jsonファイルを開く
+
+	if (!stream.is_open())
+		throw new std::exception("Failed open file.");
+
+	if (!nlohmann::json::accept(stream))
+		throw new std::exception("jsonのフォーマットが不正");
+
+	nlohmann::json j;
+	stream >> j;
+
+	try
+	{
+		T data = nlohmann::json::parse(stream).get<T>();	//parseはjsonファイルからC++で使えるように変換するもの
+		//stream.close();	//パースしたら閉じておｋ
+
+		return data;
+	}
+	catch (const std::exception&)
+	{
+		stream.close();	//パースしたら閉じておｋ
+		return T{};
+	}
 }
