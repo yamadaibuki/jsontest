2ff10597a8c8f2d73f92453b53955d153d73b4d9
OotaniMao <daguzhensheng@gmail.com>
2025-05-25 13:25:40 +0900

    jsonざっくり理解をコメント

--- test/main.cpp
+++ test/main.cpp
@@ -1,23 +1,26 @@
 #include "DxLib.h"
 #include "nlohmannJson.h"
 
+//jsonデータ用の構造体
 struct JsonData
 {
-	int hpMax;
-	int hpLimiteOver;
-
-	float initUIPositionX;
-	float initUIPositionY;
+	int		hpMax;
+	int		hpLimitOver;
+	float	initUIPositionX;
+	float	initUIPositionY;
 };
 
+//任意の型変換(第一引数→nlohmannjson、第二引数→jsonのデータ)
 inline void from_json(const nlohmann::json& json, JsonData& jsondata)
 {
+	//at("")でデータの要素を指定してget_toで指定した要素の中身（数値など)をget_toの引数の変数に代入する
 	json.at("hp_max").get_to(jsondata.hpMax);
-	json.at("hp_limite_over").get_to(jsondata.hpLimiteOver);
+	json.at("hp_limit_over").get_to(jsondata.hpLimitOver);
 	json.at("init_ui_position_x").get_to(jsondata.initUIPositionX);
 	json.at("init_ui_position_y").get_to(jsondata.initUIPositionY);
 }
 
+//hp描画関数
 void DrawHP(int hp, int hpMax, VECTOR initPosition)
 {
 	int color = GetColor(255, 255, 255);
@@ -27,23 +30,27 @@ void DrawHP(int hp, int hpMax, VECTOR initPosition)
 		initPosition.x + 200 * hp / hpMax, initPosition.y + 20, color, TRUE);	//HPゲージを描画
 }
 
-int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
 {
 	ChangeWindowMode(TRUE), DxLib_Init(), SetDrawScreen(DX_SCREEN_BACK); //ウィンドウモード変更と初期化と裏画面設定;
 
 	int currentHp = 0;
 
+
+	//不具合:LoadDataを使わずにこっちを使うと正常に動く。なんで？？？？
 	std::ifstream stream("data.json");
 	JsonData jsonData = nlohmann::json::parse(stream).get<JsonData>();
-	//JsonData jsonData = LoadData<JsonData>("data.json");
-	VECTOR position;
-	position.x = jsonData.initUIPositionX;
-	position.y = jsonData.initUIPositionY;
-	position.z = 0.0f;
 
+	//不具合:正常に動かずプログラムの途中で落ちる。parseエラー。nlohmannJson.hがどっかおかしいんやろな
+	//jsonデータ用の構造体変数にデータを読み込み。templateの<>で型をjsonデータ用の構造体にする
+	//JsonData jsonData = LoadData<JsonData>("data.json");
+
+	VECTOR position = VGet(jsonData.initUIPositionX, jsonData.initUIPositionY, 0.0);
+
+	//メインループ
 	while (!ScreenFlip() && !ProcessMessage() && !ClearDrawScreen() && !CheckHitKey(KEY_INPUT_ESCAPE)) 
 	{
-		DrawHP(currentHp, jsonData.hpMax, position);//.atは要素数を指定するもの。getでポインター渡しみたいなやつ。<>で型を指定。
+		DrawHP(currentHp, jsonData.hpMax, position);
 		currentHp++;
 		if (currentHp >= jsonData.hpMax)
 		{
--- test/nlohmannJson.h
+++ test/nlohmannJson.h
@@ -3,27 +3,34 @@
 #include <fstream>
 #include<nlohmann/json.hpp>
 
-// 書き直し！！
+// 書き直し必須！！
 
+//templateでどの型でも使えるようにする
 template<typename T>
+
+//inlineでmainに埋め込む。型はtemplateのやつにする。引数はjsonデータのファイル名が入る
 inline T LoadData(const std::string& fileName)
 {
-	std::ifstream stream(fileName);//jsonファイルを開く
+	//jsonファイルを開く
+	std::ifstream stream(fileName);
 
+	//エラー処理
 	if (!stream.is_open())
 		throw new std::exception("Failed open file.");
-
 	if (!nlohmann::json::accept(stream))
 		throw new std::exception("jsonのフォーマットが不正");
 
-	nlohmann::json j;
-	stream >> j;
+	/*nlohmann::json j;
+	stream >> j;*/
 
+	//例外処理?らしい
 	try
 	{
-		T data = nlohmann::json::parse(stream).get<T>();	//parseはjsonファイルからC++で使えるように変換するもの
-		//stream.close();	//パースしたら閉じておｋ
+		//parseはjsonファイルからC++で使えるように変換するもの。
+		T data = nlohmann::json::parse(stream).get<T>();
+		stream.close();	//パースしたら閉じておｋ
 
+		//変換したデータを返す
 		return data;
 	}
 	catch (const std::exception&)
