22359d9d2f766b999d029b3cf3b307d81a273a3f
yamadaibuki <hensati11@gmail.com>
2025-05-26 10:46:05 +0900

    try-catch文のザックリ解説をコメント

--- test/nlohmannJson.h
+++ test/nlohmannJson.h
@@ -3,35 +3,6 @@
 #include <fstream>
 #include<nlohmann/json.hpp>
 
-//templateでどの型でも使えるようにする
-template<typename T>
-
-//inlineでmainに埋め込む。型はtemplateのやつにする。引数はjsonデータのファイル名が入る
-inline T LoadData(const std::string& fileName)
-{
-	//jsonファイルを開く
-	std::ifstream stream(fileName);
-
-	//例外処理?らしい
-	try
-	{
-		//parseはjsonファイルからC++で使えるように変換するもの。
-		T data = nlohmann::json::parse(stream).get<T>();
-		stream.close();	//パースしたら閉じておｋ
-
-		//変換したデータを返す
-		return data;
-	}
-	catch (const std::exception&)
-	{
-		//エラー処理(trychach文の中に入れる)
-		if (!stream.is_open())
-			throw std::runtime_error("Failed open file.");
-		if (!nlohmann::json::accept(stream))
-			throw std::runtime_error("jsonのフォーマットが不正");
-	}
-}
-
 //jsonデータ用の構造体
 struct JsonData
 {
@@ -49,4 +20,33 @@ inline void from_json(const nlohmann::json& json, JsonData& jsondata)
 	json.at("hp_limit_over").get_to(jsondata.hpLimitOver);
 	json.at("init_ui_position_x").get_to(jsondata.initUIPositionX);
 	json.at("init_ui_position_y").get_to(jsondata.initUIPositionY);
+}
+
+//templateでどの型でも使えるようにする
+template<typename T>
+
+//inlineでmainに埋め込む。型はtemplateのやつにする。引数はjsonデータのファイル名が入る
+inline T LoadData(const std::string& fileName)
+{
+	//jsonファイルを開く
+	std::ifstream stream(fileName);
+
+	//例外処理
+	try//例外が発生するかもしれない処理
+	{
+		//parseはjsonファイルからC++で使えるように変換するもの。
+		T data = nlohmann::json::parse(stream).get<T>();
+		stream.close();	//パースしたら閉じておｋ
+
+		//変換したデータを返す
+		return data;
+	}
+	catch (const std::exception&)//例外が発生したときにどうするかの処理
+	{
+		//エラー処理(trychach文の中に入れる。throwで例外を発生させる。runtime_errorは実行時のエラー)
+		if (!stream.is_open())
+			throw std::runtime_error("Failed open file.");
+		if (!nlohmann::json::accept(stream))
+			throw std::runtime_error("jsonのフォーマットが不正");
+	}
 }
